234817682e98cd133e387747a99659ae0e6683a9
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-22 06:39:14 +0600

    added the basic hero section

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,22 @@
+
+const Banner = () => {
+    return (
+        <>
+            {/*section to hold everything  */}
+            <section className="pt-52 flex justify-between items-center">
+                {/* section to hold textx */}
+                <section>
+                    <h3 className="text-5xl mb-3 font-semibold">Your New Smile Starts <br /> Here</h3>
+                    <p className="mb-7">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the</p>
+                    <button className="text-white p-2 rounded-md bg-gradient-to-r from-[#17D2BB] to-[#11D0E2]">Get started</button>
+                </section>
+                {/* section to hold the image */}
+                <section>
+                    <img className="rounded-md" src="https://i.ibb.co/Yy0Zm3f/17818.jpg" alt="" />
+                </section>
+            </section>
+        </>
+    );
+};
+
+export default Banner;
