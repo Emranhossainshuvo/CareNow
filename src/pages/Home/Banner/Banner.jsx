b8d0f9057ab619095978fce612ba61ade97f919a
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-22 10:27:41 +0600

    banner file responsiveness done

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -3,12 +3,14 @@ const Banner = () => {
     return (
         <>
             {/*section to hold everything  */}
-            <section className="pt-52 pb-60 flex justify-between items-center">
+            <section className="pt-52 pb-60 md:flex md:justify-between md:items-center">
                 {/* section to hold textx */}
                 <section>
-                    <h3 className="text-5xl mb-3 font-semibold">Your New Smile Starts <br /> Here</h3>
-                    <p className="mb-7">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the</p>
-                    <button className="text-white p-2 rounded-md bg-gradient-to-r from-[#17D2BB] to-[#11D0E2]">Get started</button>
+                    <h3 className="text-5xl text-center md:text-start mb-3 font-semibold">Your New Smile Starts <br /> Here</h3>
+                    <p className="mb-7 text-center md:text-start">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the</p>
+                    <section className="w-full flex mb-3 justify-center">
+                        <button className="text-white  p-2 rounded-md bg-gradient-to-r from-[#17D2BB] to-[#11D0E2]">Get started</button>
+                    </section>
                 </section>
                 {/* section to hold the image */}
                 <section>
