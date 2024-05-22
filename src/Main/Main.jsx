22ba070c43d2617b16a52c1d042e38af3360dd92
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-22 06:20:46 +0600

    added the basic navbar

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/shared/Navbar/Navbar";
 
 const Main = () => {
     return (
-        <div>
-                <Outlet />
+        <div className="max-w-screen-2xl mx-auto">
+            <Navbar />
+            <Outlet />
         </div>
     );
 };
---
+++ src/components/shared/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleToggle = () => {
+        setMenuOpen(!menuOpen)
+    };
+
+
+    return (
+        <>
+            {/* main div to hold everything in the navbar file */}
+            <div className="flex justify-between pt-7 mb-6 p-2 items-center">
+                {/* div for left side element such as name and icon */}
+                <div>
+                    <h3 className="text-2xl font-semibold">CareNow</h3>
+                </div>
+
+                <div className={`menu-links absolute md:static top-0 right-5 md:flex ${menuOpen ? 'block' : 'hidden'} text-lg font-medium`}>
+                    <ul className="md:flex items-center gap-5">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Appointment</li>
+                        <li>Reviews</li>
+                        <li>Contact us</li>
+                        <li>Login</li>
+                    </ul>
+                </div>
+                <div className="menu-icon md:hidden" onClick={handleToggle}>
+                    {menuOpen ? <FaTimes /> : <FaBars />}
+                </div>
+
+            </div>
+        </>
+    );
+};
+
+export default Navbar;
