94b5eb7ee18205eb1453c7791567ef8ec15710c8
EmranHossain <emran.shuvo.dev@gmail.com>
2024-06-04 20:45:08 +0600

    installed firebase and added firebaswe config file

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA18RLkXxLfv1S1ihH5wfk-rbRsJ7I0TPs",
+  authDomain: "carenow-19c5f.firebaseapp.com",
+  projectId: "carenow-19c5f",
+  storageBucket: "carenow-19c5f.appspot.com",
+  messagingSenderId: "5006658660",
+  appId: "1:5006658660:web:fed46cc27014515e806371"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
