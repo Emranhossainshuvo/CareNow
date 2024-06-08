d0d032c51427e3bd4b281c3f1644c74fdd6788f2
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-08 19:41:01 +0600

    testimonials added by the json file

--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -1,28 +1,18 @@
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import { useEffect, useState } from 'react';
+import TestimonialsCard from './TestimonialsCard';
 
 
 const Testimonials = () => {
 
-    const responsive = {
-        superLargeDesktop: {
-            // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
-        }
-    };
+
+
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch('reviews.json')
+        .then(res => res.json())
+        .then(data => setReviews(data))
+    }, [])
 
 
     return (
@@ -37,162 +27,16 @@ const Testimonials = () => {
                 {/* section that will hold the testimonials cards */}
                 <section>
 
-                    <Carousel className='mb-40 p-5' responsive={responsive}>
-                        <div className='text-xs rounded-md shadow-black shadow-md me-2 p-8'>
-                            <p className='mb-9'>It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</p>
-                            <div className='flex items-center gap-5'>
-                                <div className="avatar">
-                                    <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                        <img src="https://i.ibb.co/93DgzVn/female-doctor-hospital.png" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <h4 className='text-[#3A4256] text-lg font-semibold'>Winson Herry</h4>
-                                    <p>California</p>
-                                </div>
-                            </div>
+                        <div className=''>
+                        {
+                                reviews.map(
+                                    review => <TestimonialsCard
+                                    key={review.userName}
+                                    reviews={review}
+                                    ></TestimonialsCard>
+                                )
+                        }
                         </div>
-                        <div className='text-xs rounded-md shadow-black shadow-md me-2 p-8'>
-                            <p className='mb-9'>It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</p>
-                            <div className='flex items-center gap-5'>
-                                <div className="avatar">
-                                    <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                        <img src="https://i.ibb.co/93DgzVn/female-doctor-hospital.png" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <h4 className='text-[#3A4256] text-lg font-semibold'>Winson Herry</h4>
-                                    <p>California</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='text-xs rounded-md shadow-black shadow-md me-2 p-8'>
-                            <p className='mb-9'>It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</p>
-                            <div className='flex items-center gap-5'>
-                                <div className="avatar">
-                                    <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                        <img src="https://i.ibb.co/93DgzVn/female-doctor-hospital.png" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <h4 className='text-[#3A4256] text-lg font-semibold'>Winson Herry</h4>
-                                    <p>California</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='text-xs rounded-md shadow-black shadow-md me-2 p-8'>
-                            <p className='mb-9'>It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</p>
-                            <div className='flex items-center gap-5'>
-                                <div className="avatar">
-                                    <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                        <img src="https://i.ibb.co/93DgzVn/female-doctor-hospital.png" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <h4 className='text-[#3A4256] text-lg font-semibold'>Winson Herry</h4>
-                                    <p>California</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='text-xs rounded-md shadow-black shadow-md me-2 p-8'>
-                            <p className='mb-9'>It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</p>
-                            <div className='flex items-center gap-5'>
-                                <div className="avatar">
-                                    <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                        <img src="https://i.ibb.co/93DgzVn/female-doctor-hospital.png" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <h4 className='text-[#3A4256] text-lg font-semibold'>Winson Herry</h4>
-                                    <p>California</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='text-xs rounded-md shadow-black shadow-md me-2 p-8'>
-                            <p className='mb-9'>It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</p>
-                            <div className='flex items-center gap-5'>
-                                <div className="avatar">
-                                    <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                        <img src="https://i.ibb.co/93DgzVn/female-doctor-hospital.png" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <h4 className='text-[#3A4256] text-lg font-semibold'>Winson Herry</h4>
-                                    <p>California</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='text-xs rounded-md shadow-black shadow-md me-2 p-8'>
-                            <p className='mb-9'>It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</p>
-                            <div className='flex items-center gap-5'>
-                                <div className="avatar">
-                                    <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                        <img src="https://i.ibb.co/93DgzVn/female-doctor-hospital.png" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <h4 className='text-[#3A4256] text-lg font-semibold'>Winson Herry</h4>
-                                    <p>California</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='text-xs rounded-md shadow-black shadow-md me-2 p-8'>
-                            <p className='mb-9'>It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</p>
-                            <div className='flex items-center gap-5'>
-                                <div className="avatar">
-                                    <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                        <img src="https://i.ibb.co/93DgzVn/female-doctor-hospital.png" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <h4 className='text-[#3A4256] text-lg font-semibold'>Winson Herry</h4>
-                                    <p>California</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='text-xs rounded-md shadow-black shadow-md me-2 p-8'>
-                            <p className='mb-9'>It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</p>
-                            <div className='flex items-center gap-5'>
-                                <div className="avatar">
-                                    <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                        <img src="https://i.ibb.co/93DgzVn/female-doctor-hospital.png" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <h4 className='text-[#3A4256] text-lg font-semibold'>Winson Herry</h4>
-                                    <p>California</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='text-xs rounded-md shadow-black shadow-md me-2 p-8'>
-                            <p className='mb-9'>It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</p>
-                            <div className='flex items-center gap-5'>
-                                <div className="avatar">
-                                    <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                        <img src="https://i.ibb.co/93DgzVn/female-doctor-hospital.png" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <h4 className='text-[#3A4256] text-lg font-semibold'>Winson Herry</h4>
-                                    <p>California</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='text-xs rounded-md shadow-black shadow-md me-2 p-8'>
-                            <p className='mb-9'>It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</p>
-                            <div className='flex items-center gap-5'>
-                                <div className="avatar">
-                                    <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                        <img src="https://i.ibb.co/93DgzVn/female-doctor-hospital.png" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <h4 className='text-[#3A4256] text-lg font-semibold'>Winson Herry</h4>
-                                    <p>California</p>
-                                </div>
-                            </div>
-                        </div>
-                    </Carousel>
 
                 </section>
             </section>
---
+++ src/pages/Home/Testimonials/TestimonialsCard.jsx
@@ -0,0 +1,27 @@
+
+const TestimonialsCard = ({reviews}) => {
+
+    const {userName, img, from , review} = reviews || {};
+
+    console.log(reviews)
+    return (
+        <>
+         <div className='text-xs rounded-md shadow-black shadow-md me-2 p-8'>
+                            <p className='mb-9'>{review}</p>
+                            <div className='flex items-center gap-5'>
+                                <div className="avatar">
+                                    <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                        <img src="https://i.ibb.co/93DgzVn/female-doctor-hospital.png" />
+                                    </div>
+                                </div>
+                                <div>
+                                    <h4 className='text-[#3A4256] text-lg font-semibold'>{userName}</h4>
+                                    <p>{from}</p>
+                                </div>
+                            </div>
+                        </div>   
+        </>
+    );
+};
+
+export default TestimonialsCard;
